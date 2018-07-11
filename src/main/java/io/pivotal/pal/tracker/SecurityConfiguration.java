02296cd6eaf3ef934698be41902af2ad277c807a
Jeff.Black <thomas.black@accenturefederal.com>
2018-07-11 15:01:03 -0600

    Security lab

---
+++ src/main/java/io/pivotal/pal/tracker/SecurityConfiguration.java
@@ -0,0 +1,37 @@
+package io.pivotal.pal.tracker;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    private boolean httpsDisabled;
+
+    SecurityConfiguration(@Value("${https.disabled}") boolean httpsDisabled) {
+        this.httpsDisabled = httpsDisabled;
+    }
+
+    @Autowired
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser("user").password("password").roles("USER");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        if (!httpsDisabled) {
+            http.requiresChannel().anyRequest().requiresSecure();
+        }
+        http.authorizeRequests().antMatchers("/**").hasRole("USER").and().httpBasic().and().csrf().disable();
+    }
+
+
+}
